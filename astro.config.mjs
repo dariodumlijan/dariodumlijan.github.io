4050037e136ef55a4b5569dad2975faf092a93bc
Dario Dumlijan <dario.dumlijan1@gmail.com>
2024-11-26 23:46:38 +0100

    build: config astro github pages build

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
+  site: 'https://dariodumlijan.com',
   integrations: [
     icon({
       iconDir: 'src/assets/icons'
