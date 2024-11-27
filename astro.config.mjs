78d98404ad2a2ed8a6196431afbfad0e01aa128b
Dario Dumlijan <dario.dumlijan1@gmail.com>
2024-11-27 11:56:44 +0100

    feat: add sitemap & robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,6 +10,11 @@ export default defineConfig({
   integrations: [
     icon({
       iconDir: 'src/assets/icons'
+    }),
+    sitemap({
+      priority: 0.7,
+      changefreq: 'monthly',
+      lastmod: new Date(),
     }),
   ],
   vite: {
---
+++ src/pages/robots.txt.ts
@@ -0,0 +1,10 @@
+import type { APIRoute } from 'astro';
+
+const getRobotsTxt = (sitemapURL: URL) => {
+  return `User-agent: *\nAllow: /\n\nSitemap: ${sitemapURL.href}`
+};
+
+export const GET: APIRoute = ({ site }) => {
+  const sitemapURL = new URL('sitemap-index.xml', site);
+  return new Response(getRobotsTxt(sitemapURL));
+};
