2ac0aabf5d3bc427c970a3f904faa3f229b29a0b
Dario Dumlijan <dario.dumlijan1@gmail.com>
2026-01-12 18:00:01 +0100

    chore: linter fix

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,4 +1,4 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 const home = defineCollection({
 	type: 'content',
@@ -6,23 +6,23 @@ const home = defineCollection({
 		// bio.md
 		component: z.string().optional(),
 		// misc.md
-		experience:  z.object({
+		experience: z.object({
 			component: z.string()
 		}).optional(),
-		featured:  z.object({
+		featured: z.object({
 			component: z.string(),
 			cta: z.string()
 		}).optional(),
-		project:  z.object({
+		project: z.object({
 			component: z.string(),
 			cta: z.string()
 		}).optional(),
 		// skills.md
-		sortBy:  z.object({
+		sortBy: z.object({
 			label: z.string(),
 			option: z.string()
 		}).optional(),
-		skills:  z.array(z.object({
+		skills: z.array(z.object({
 			language: z.string(),
 			link: z.string(),
 			year: z.number()
@@ -35,11 +35,11 @@ const home = defineCollection({
 		// profile.md - component
 		name: z.string().optional(),
 		location: z.string().optional(),
-		cta:  z.object({
+		cta: z.object({
 			label: z.string(),
 			value: z.string()
 		}).optional(),
-		socials:  z.array(z.object({
+		socials: z.array(z.object({
 			icon: z.string(),
 			label: z.string(),
 			value: z.string()
